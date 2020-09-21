1cd89a1128c5aa5f1ec77492ab24e42c6c9f12c5
Andre Przywara <andre.przywara@arm.com>
2020-09-21 14:46:01 +0100

    libfdt: fdt_add_string_(): Fix comparison warning
    With -Wsign-compare, compilers warn about a mismatching signedness
    in a comparison in fdt_add_string_().
    
    As struct_top can only be positive, just use an unsigned type for it,
    and avoid the signedness difference.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- libfdt/fdt_sw.c
+++ libfdt/fdt_sw.c
@@ -249,7 +249,8 @@ static int fdt_add_string_(void *fdt, const char *s)
 	char *strtab = (char *)fdt + fdt_totalsize(fdt);
 	int strtabsize = fdt_size_dt_strings(fdt);
 	int len = strlen(s) + 1;
-	int struct_top, offset;
+	unsigned int struct_top;
+	int offset;
 
 	offset = -strtabsize - len;
 	struct_top = fdt_off_dt_struct(fdt) + fdt_size_dt_struct(fdt);
